d53fb8834fbb2e1a11fc1a4ddd39e89535dd4328
elizaveta-rybina <liza.rybina@inbox.ru>
2024-11-16 22:22:24 +0300

    add constant settings

---
+++ src/app/consts/colors.ts
@@ -0,0 +1,18 @@
+export const LIGHT_THEME_COLORS = {
+  textPrimary: "#03060C",
+  textSecondary: "#2A4A6A",
+  textTertiary: "#96AEC4",
+  accentPrimary: "#2A59AE",
+  accentSecondary: "#6D96CE",
+  background: "#FFFFFF",
+};
+
+export const DARK_THEME_COLORS = {
+  textPrimary: "#E1E8ED",
+  textSecondary: "#94A3B8",
+  textTertiary: "#64748B",
+  accentPrimary: "#3B82F6",
+  accentSecondary: "#60A5FA",
+  background: "#0F172A",
+};
+
