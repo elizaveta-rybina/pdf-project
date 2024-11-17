5d95e1a06d773a0805c284c5b93884795940eae0
elizaveta-rybina <liza.rybina@inbox.ru>
2024-11-17 20:42:59 +0300

    add login button

---
+++ src/entities/LoginButton/ui/LoginButton.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next'
+import styles from '../styles/LoginButton.module.scss';
+
+export const LoginButton = () => {
+	const { t } = useTranslation();
+	const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/login');
+  };
+
+	return (
+		<button onClick={handleClick} className={styles.button}>
+			<div>
+				{t('header.login')}
+			</div>
+		</button>
+	)
+}
