c4de2b63e4fc2207bc2a9a8f061e1124bb522311
elizaveta-rybina <liza.rybina@inbox.ru>
2024-12-03 20:13:01 +0300

    Added wrapper for information boards

---
+++ src/widgets/BoardsList/cfg/boardsData.tsx
@@ -0,0 +1,78 @@
+import {
+	FaCompress,
+	FaFilePdf,
+	FaImage,
+	FaPowerOff,
+	FaRegFileAlt,
+	FaRegFileImage,
+} from 'react-icons/fa'
+import { FaArrowsSplitUpAndLeft } from 'react-icons/fa6'
+
+import { IconType } from 'react-icons'
+
+export interface BoardData {
+	active: boolean
+	title: string
+	description: string
+	link: string
+	icon: IconType
+}
+
+export const boardsData: BoardData[] = [
+	{
+		active: true,
+		title: 'boardsTitle.merge',
+		description: 'boardsDescription.merge',
+		link: '/merge',
+		icon: FaRegFileAlt,
+	},
+	{
+		active: false,
+		title: 'boardsTitle.split',
+		description: 'boardsDescription.split',
+		link: '/split',
+		icon: FaArrowsSplitUpAndLeft,
+	},
+	{
+		active: true,
+		title: 'boardsTitle.compress',
+		description: 'boardsDescription.compress',
+		link: '/compress',
+		icon: FaCompress,
+	},
+	{
+		active: true,
+		title: 'boardsTitle.wordToPDF',
+		description: 'boardsDescription.wordToPDF',
+		link: '/wordToPDF',
+		icon: FaFilePdf,
+	},
+	{
+		active: false,
+		title: 'boardsTitle.PDFToPowerPoint',
+		description: 'boardsDescription.PDFToPowerPoint',
+		link: '/PDFToPowerPoint',
+		icon: FaPowerOff,
+	},
+	{
+		active: false,
+		title: 'boardsTitle.PowerPointToPDF',
+		description: 'boardsDescription.PowerPointToPDF',
+		link: '/PowerPointToPDF',
+		icon: FaRegFileAlt,
+	},
+	{
+		active: true,
+		title: 'boardsTitle.JPGToPDF',
+		description: 'boardsDescription.JPGToPDF',
+		link: '/JPGToPDF',
+		icon: FaImage,
+	},
+	{
+		active: true,
+		title: 'boardsTitle.PDFToJPG',
+		description: 'boardsDescription.PDFToJPG',
+		link: '/PDFToJPG',
+		icon: FaRegFileImage,
+	},
+]
