86d87c0a5945f48c29edd5bb1baa69fb9975db7e
elizavetarybina <liza.rybina@inbox.ru>
2024-12-14 14:07:54 +0300

    #feat: add compress page

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,7 +1,7 @@
-import React, {lazy} from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Layout } from 'shared/ui/Layout';
-import { MainPage } from 'pages/MainPage';
+import { CompressPage } from 'pages/CompressPage'
+import { MainPage } from 'pages/MainPage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Layout } from 'shared/ui/Layout'
 
 export const routes = [
   {
@@ -9,6 +9,14 @@ export const routes = [
     element: (
       <Layout>
         <MainPage />
+      </Layout>
+    ),
+  },
+  {
+    path: '/compress',
+    element: (
+      <Layout>
+        <CompressPage />
       </Layout>
     ),
   },
---
+++ src/pages/CompressPage/ui/CompressPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CompressPage = () => {
+	return (
+		<div>CompressPage</div>
+	)
+}
