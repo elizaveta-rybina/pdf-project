5900cd9314ea315188156e95e2a8e3ac1333caed
elizavetarybina <liza.rybina@inbox.ru>
2024-12-14 13:20:04 +0300

    #style: add css for merge page

--- src/pages/MergePage/ui/MergePage.tsx
+++ src/pages/MergePage/ui/MergePage.tsx
@@ -2,10 +2,14 @@ import { FileUploader } from 'features/FileUploader'
 import { MergeButton } from 'features/MergeButton'
 import { useTranslation } from 'react-i18next'
 
+import styles from '../styles/MergePage.module.scss'
+
 export const MergePage = () => {
 	const { t } = useTranslation()
 	return(
 		<>
+			<h1 className={styles.title}>{t('mergePage.title')}</h1>
+			<p className={styles.text}>{t('mergePage.text')}</p>
 			<MergeButton/>
 			<FileUploader/>
 		</>
