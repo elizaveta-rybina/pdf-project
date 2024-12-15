d3dca43b4b8b842b37871e0001f7b4b33f4ddd96
elizavetarybina <liza.rybina@inbox.ru>
2024-12-15 21:00:27 +0300

    #feat: add function for displaying notifications

---
+++ src/entities/Toast/ui/ToastNotification.tsx
@@ -0,0 +1,28 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface ToastNotificationProps {
+  title: string;
+  text: string;
+  type: 'success' | 'error' | 'warning' | 'info' | 'default';
+}
+
+export const showToast = ({ title, text, type }: ToastNotificationProps) => {
+  switch (type) {
+    case 'success':
+      toast.success(<div><strong>{title}</strong><br />{text}</div>);
+      break;
+    case 'error':
+      toast.error(<div><strong>{title}</strong><br />{text}</div>);
+      break;
+    case 'warning':
+      toast.warning(<div><strong>{title}</strong><br />{text}</div>);
+      break;
+    case 'info':
+      toast.info(<div><strong>{title}</strong><br />{text}</div>);
+      break;
+    default:
+      toast(<div><strong>{title}</strong><br />{text}</div>);
+      break;
+  }
+};
